d9f98e670b80eaa324804bf4fe82b97c0867cd6d
David Lowry <DavidLowry64@gmail.com>
2021-10-04 12:43:17 -0400

    Initial Commit

---
+++ javaObject/src/javaObject/objectClass.java
@@ -0,0 +1,11 @@
+package javaObject;
+
+public class objectClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
