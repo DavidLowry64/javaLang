5af3d89381f4e83b9d2652e09eecd1263c1e6283
David Lowry <DavidLowry64@gmail.com>
2021-10-04 12:44:53 -0400

    Second Commit

--- javaObject/src/javaObject/objectClass.java
+++ javaObject/src/javaObject/objectClass.java
@@ -1,10 +1,16 @@
 package javaObject;
 
+import java.util.*;
+
 public class objectClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+	   // Date Clone for the Obect Class
+		
+		  Date date = new Date();
+	        System.out.println(date.toString());
+	        Date date2 = (Date) date.clone();
+	        System.out.println(date2.toString());
 
 	}
 
