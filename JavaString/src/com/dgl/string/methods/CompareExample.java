83c7a51b5143a6a52900e85f9f560af352a90be3
David Lowry <DavidLowry64@gmail.com>
2021-10-04 16:55:16 -0400

    Last Commit

---
+++ JavaString/src/com/dgl/string/methods/CompareExample.java
@@ -0,0 +1,72 @@
+package com.dgl.string.methods;
+
+public class CompareExample {
+
+	public static void main(String[] args) {
+		
+		compareExampleOne();
+		compareExampleTwo();
+		compareIgnoreCaseOne();
+		compareIgnoreCaseTwo();
+		
+		
+	}
+	
+		public static void compareExampleOne() {
+			String s1 = "Welcome to TTRC";
+			String s2 = "Welcome to TTRC";
+			String s3 = "Hello";
+			String s4 = "World";
+			System.out.println(s1.compareTo(s2));// 0 because both are equal
+			System.out.println(s1.compareTo(s3));// -2 because "H" is 2 times lower
+													// than "J"
+			System.out.println(s1.compareTo(s4));// 1 because "G" is 1 times greater
+													// than "H"
+		}
+		
+		public static void compareExampleTwo() {
+			String s1 = "Hello";
+			String s2 = "";
+			String s3 = "Hi";
+
+			// compare with empty string, returns length of the string
+			System.out.println(s1.compareTo(s2));
+
+			// If first string is empty, result would be negative
+			System.out.println(s2.compareTo(s3));
+		}
+		
+		
+		public static void compareIgnoreCaseOne() {
+			String s1="HELLO WORLD";  
+			String s2="hello world";  
+			String s3="JAVA";  
+			String s4="java";  
+			System.out.println("The first string is " + s1);
+			System.out.println("The second string is " + s2);
+			System.out.println("Compare first String to the second string " + s1.compareToIgnoreCase(s2));
+			System.out.println("The third string is " + s3);
+			System.out.println("The fourth string is " + s4);
+			System.out.println("Compare of the 3rd and 4th strings " + s3.compareToIgnoreCase(s4));  
+		}
+
+		// Compare different and empty strings
+		public static void compareIgnoreCaseTwo() {
+			String s1="hello";  
+			String s2="";  
+			String s3="me";  
+			
+			// compare with empty string, returns length of the string
+			System.out.println("The first string is " + s1);
+			System.out.println("The second string is actually blank empty " + s2);
+			System.out.println("The third string is " + s3);
+			
+			System.out.println("The first string to the second one " + s1.compareToIgnoreCase(s2));
+			
+			
+			System.out.println("The second string to the third string " + s2.compareToIgnoreCase(s3)); 
+		}
+
+	
+
+}
--- JavaString/src/com/dgl/string/methods/UniCodeExample.java
+++ JavaString/src/com/dgl/string/methods/UniCodeExample.java
@@ -8,9 +8,22 @@ public class UniCodeExample {
 		int unicode = str.codePointAt(0);
 		System.out.println("the character (Unicode code point) at the specified index is :: " + unicode);
 
+		// Unicode conversion example
 		String myString = "\u0048\u0065\u006C\u006C\u006F World";
 		System.out.println("The example to convert unicode into a word " + myString);
 		
+		// Code Before Example
+		int unicodeBefore = str.codePointBefore(11);
+		System.out.println("Unicode code point before ndex number " + unicodeBefore);
+		
+		// The length of the string and count at a specific interval
+		System.out.println("length of the string :: " + str.length());
+		int unicodePoint = str.codePointCount(10, str.length());
+		System.out.println("Unicode code point at index  " + unicodePoint);
+
+	
+	
+	
 	}
 
 }
